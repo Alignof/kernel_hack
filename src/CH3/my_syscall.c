157c912e9128578af38ccd4773b50da262ee009c
Alignof <neuromancer_wg@outlook.jp>
2022-05-14 01:43:50 +0900

    [add] define syscall swapdir

--- src/CH3/my_syscall.c
+++ src/CH3/my_syscall.c
@@ -1,6 +1,16 @@
 #include <linux/syscall.h>
 
-SYSCALL_DEFINE1(mysyscall, int, x) {
-    printk("hello world!");
-    return x;
+// https://elixir.bootlin.com/linux/v5.17.1/source/tools/include/nolibc/nolibc.h#L210
+#ifndef AT_FDCWD
+#define AT_FDCWD    -100
+#endif
+
+// https://elixir.bootlin.com/linux/v5.17.1/source/fs/namei.c#L4097
+SYSCALL_DEFINE2(swapdir, const char __user *, dir1, const char __user *, dir2) {
+    return do_swapdir(AT_FDCWD, dir1, dir2);
+}
+
+int do_swapdir(int dfd, struct filename *dir1, struct filename *dir2) {
+    printk("did swapdir");
+    return 42;
 }
