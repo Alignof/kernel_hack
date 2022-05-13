52e42442468b9b314294061439e3b5fb520053b4
Alignof <neuromancer_wg@outlook.jp>
2022-05-14 02:38:19 +0900

    [fix] fix my_syscall.c

--- src/CH3/my_syscall.c
+++ src/CH3/my_syscall.c
@@ -1,16 +1,17 @@
-#include <linux/syscall.h>
+#include <linux/syscalls.h>
 
 // https://elixir.bootlin.com/linux/v5.17.1/source/tools/include/nolibc/nolibc.h#L210
 #ifndef AT_FDCWD
 #define AT_FDCWD    -100
 #endif
 
-// https://elixir.bootlin.com/linux/v5.17.1/source/fs/namei.c#L4097
-SYSCALL_DEFINE2(swapdir, const char __user *, dir1, const char __user *, dir2) {
-    return do_swapdir(AT_FDCWD, dir1, dir2);
-}
-
 int do_swapdir(int dfd, struct filename *dir1, struct filename *dir2) {
     printk("did swapdir");
     return 42;
 }
+
+// https://elixir.bootlin.com/linux/v5.17.1/source/fs/namei.c#L4097
+SYSCALL_DEFINE2(swapdir, const char __user *, dir1, const char __user *, dir2) {
+    return do_swapdir(AT_FDCWD, getname_kernel(dir1), getname_kernel(dir2));
+}
+
