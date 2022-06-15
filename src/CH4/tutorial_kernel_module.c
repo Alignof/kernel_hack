722a52cb7b856f6dadbc81d63a59556a31848f9b
Alignof <neuromancer_wg@outlook.jp>
2022-06-15 14:50:08 +0900

    [update] implement open, close, read, write

--- src/CH4/tutorial_kernel_module.c
+++ src/CH4/tutorial_kernel_module.c
@@ -1,8 +1,70 @@
 #include <linux/module.h>
-#define DRIVER_NAME "MyDevice_NAME"
-#define DRIVER_MAJOR 63
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#define DRIVER_NAME "MyDevice"
+#define BUF_SIZE 256
+
+static const unsigned int MINOR_BASE = 0;
+static const unsigned int MINOR_NUM  = 3;
+static unsigned int major_num;
+static struct cdev my_cdev;
 
 MODULE_LICENSE("Dual BSD/GPL");
+
+struct _mydevice_file_data {
+    unsigned char buffer[BUF_SIZE];
+};
+
+static int mydevice_open(struct inode *inode, struct file *fp) {
+    printk("open my device\n");
+
+    struct _mydevice_file_data *p = kmalloc(sizeof(struct _mydevice_file_data), GFP_KERNEL);
+    if (p == NULL) {
+        printk(KERN_ERR  "kmalloc\n");
+        return -ENOMEM;
+    }
+
+    strlcat(p->buffer, "init", 4);
+    fp->private_data = p;
+
+    return 0;
+}
+
+static int mydevice_close(struct inode *inode, struct file *fp) {
+    printk("close my device\n");
+
+    if (fp->private_data) {
+        kfree(fp->private_data);
+        fp->private_data = NULL;
+    }
+
+    return 0;
+}
+
+static ssize_t mydevice_read(struct file *fp, char __user *_buf, size_t count, loff_t *f_pos) {
+    printk("read my device\n");
+
+    if (count > BUF_SIZE) count = BUF_SIZE;
+
+    struct _mydevice_file_data *p = fp->private_data;
+    if (copy_to_user(_buf, p->buffer, count) != 0) {
+        return -EFAULT;
+    }
+    return count;
+}
+
+static ssize_t mydevice_write(struct file *fp, const char __user *_buf, size_t count, loff_t *f_pos) {
+    printk("write my device\n");
+
+    struct _mydevice_file_data *p = fp->private_data;
+    if (copy_from_user(p->buffer, _buf, count) != 0) {
+        return -EFAULT;
+    }
+    return count;
+}
 
 struct file_operations s_mydevice_fops = {
     .open    = mydevice_open,
@@ -11,41 +73,35 @@ struct file_operations s_mydevice_fops = {
     .write   = mydevice_write,
 };
 
-static int mydevice_open(struct inode *inode, struct file *fp) {
-    printk("open my device\n");
-    return 0;
-}
-
-static int mydevice_close(struct inode *inode, struct file *fp) {
-    printk("close my device\n");
-    return 0;
-}
-
-static int mydevice_read(struct file *fp, char __user *_buf, size_t count, loff_t *f_pos) {
-    printk("read my device\n");
-
-    char *buf = kmalloc(sizeof(char) * count, GFP_KERNEL);
-    if (copy_from_user(buf, _buf, sizeof(char) * count) > 0) return EFAULT;
-    buf[0] = 'A';
-    if (copy_to_user(_buf, buf, sizeof(char) * count) > 0) return EFAULT;
-
-    return 1;
-}
-
-static int mydevice_write(struct file *fp, char __user *_buf, size_t count, loff_t *f_pos) {
-    printk("write my device\n");
-    return 1;
-}
-
 static int mydevice_init(void) {
     printk("init my device\n");
-    register_chrdev(DRIVER_MAJOR, DRIVER_NAME, &s_myDevice_fops);
+
+    dev_t dev;
+    if (alloc_chrdev_region(&dev, MINOR_BASE, MINOR_NUM, DRIVER_NAME) != 0) {
+        printk(KERN_ERR  "alloc_chrdev_region = %d\n", major_num);
+        return -1;
+    }
+
+    major_num = MAJOR(dev);
+    dev = MKDEV(major_num, MINOR_BASE);
+
+    cdev_init(&my_cdev,&s_mydevice_fops);
+    my_cdev.owner = THIS_MODULE;
+    int cdev_err = cdev_add(&my_cdev, dev, MINOR_NUM);
+    if (cdev_err != 0) {
+        printk(KERN_ERR  "cdev_add = %d\n", cdev_err);
+        unregister_chrdev_region(dev, MINOR_NUM);
+        return -1;
+    }
     return 0;
 }
 
 static void mydevice_exit(void) {
     printk("exit my device\n");
-    unregister_chrdev(DRIVER_MAJOR, DRIVER_NAME);
+
+    dev_t dev = MKDEV(major_num, MINOR_BASE);
+    cdev_del(&my_cdev);
+    unregister_chrdev_region(dev, MINOR_NUM);
 }
 
 module_init(mydevice_init);
