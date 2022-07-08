4c3913cdcb2a35342777b123143ef433d2b39c24
Alignof <neuromancer_wg@outlook.jp>
2022-07-08 17:57:42 +0900

    [wip][update] add cpu freq

--- src/CH5/my_procfs.c
+++ src/CH5/my_procfs.c
@@ -5,6 +5,7 @@
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
 #include <linux/cpufreq.h>
+#include <x86/kernel/cpu/cpu.h>
 #include <asm/processor.h>
 #include "my_procfs.h"
 
@@ -13,37 +14,11 @@
 #define BUF_SIZE 256
 
 static unsigned int major_num;
-static unsigned int read_ok = 1;
-static char buffer[BUF_SIZE];
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int mydevice_open(struct inode *inode, struct file *fp) {
     printk("open my device\n");
-
-    int cpu_id = 0;
-	struct cpuinfo_x86 *c = &cpu_data(cpu_id);
-
-    snprintf(
-        buffer, 
-        BUF_SIZE, 
-        "vender id: %s\n"
-        "cpu family: %u\n"
-        "model: %s\n"
-        "cpu MHz: %u\n"
-        "cache size: %u KB\n"
-        "core id: %u\n"
-        "cpu cores: %u\n",
-        c->x86_vendor_id,
-        c->x86,
-        c->x86_model_id,
-        cpufreq_generic_get(cpu_id),
-        c->x86_cache_size,
-        c->cpu_core_id,
-        c->x86_max_cores
-    );
-
-    read_ok = 1;
     return 0;
 }
 
@@ -54,28 +29,57 @@ static int mydevice_close(struct inode *inode, struct file *fp) {
 }
 
 static ssize_t mydevice_read(struct file *fp, char __user *_buf, size_t count, loff_t *f_pos) {
-    if (read_ok) {
-        printk("read my device\n");
+    char buffer[BUF_SIZE];
+    int cpu_id = 0;
+    struct cpuinfo_x86 *c = &cpu_data(cpu_id);
 
-        if (count > BUF_SIZE) count = BUF_SIZE-1;
-        sprintf(_buf, "%s\n", buffer);
+    unsigned int freq;
+    if (cpu_has(c->cpu_index, X86_FEATURE_TSC)) {
+        freq = aperfmperf_get_khz(cpu_id);
 
-        read_ok = 0;
-    } else {
-        return 0;
+        if (!freq)
+            freq = cpufreq_quick_get(cpu_id);
+        if (!freq)
+            freq = cpu_khz;
     }
 
-    return count;
+    int size = snprintf(
+        buffer, 
+        BUF_SIZE, 
+        "vender id: %s\n"
+        "cpu family: %u\n"
+        "model: %s\n"
+        "cpu MHz: %u.%03u\n"
+        "cache size: %u KB\n"
+        "core id: %u\n"
+        "cpu cores: %u\n",
+        c->x86_vendor_id,
+        c->x86,
+        c->x86_model_id,
+        freq / 1000, (freq % 1000),
+        c->x86_cache_size,
+        c->cpu_core_id,
+        c->x86_max_cores
+    );
+
+    printk("read my device\n");
+
+    if (count > BUF_SIZE) count = BUF_SIZE-1;
+    copy_to_user(_buf, buffer, size);
+
+    return 0;
 }
 
 static ssize_t mydevice_write(struct file *fp, const char __user *_buf, size_t count, loff_t *f_pos) {
     printk("write my device\n");
 
+    /*
     if (count > BUF_SIZE) count = BUF_SIZE-1;
     if (copy_from_user(buffer, _buf, count) != 0) {
         return -EFAULT;
     }
     buffer[count] = '\0';
+    */
     return count;
 }
 
