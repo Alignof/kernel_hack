0fbac739d21ffec332856838c60a554042c1f639
Alignof <neuromancer_wg@outlook.jp>
2022-08-19 02:11:25 +0900

    [wip][fix] fix program of CH4

--- src/CH4/my_chdev.c
+++ src/CH4/my_chdev.c
@@ -11,12 +11,13 @@
 static const unsigned int MINOR_BASE = 0;
 static const unsigned int MINOR_NUM  = 3;
 static unsigned int major_num;
+static dev_t dev;
 static struct cdev my_cdev;
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct _mydevice_file_data {
-    int *pos;
+    int pos;
     unsigned char buffer[BUF_SIZE];
     struct parameter param;
 };
@@ -58,10 +59,9 @@ static int mydevice_open(struct inode *inode, struct file *fp) {
         return -ENOMEM;
     }
 
-    strlcat(p->buffer, "init", 5);
-    int *buf_pos = kmalloc(sizeof(int), GFP_KERNEL);
-    *buf_pos = 5;
-    p->pos = buf_pos;
+    char *init_msg = "init";
+    strncpy(p->buffer, init_msg, strlen(init_msg)+1);
+    p->pos = strlen(init_msg)+1;
     p->param.value1 = 0;
     p->param.value2 = 0;
     fp->private_data = p;
@@ -83,20 +83,18 @@ static ssize_t mydevice_read(struct file *fp, char __user *_buf, size_t count, l
     printk("read my device\n");
 
     struct _mydevice_file_data *p = fp->private_data;
-    int *buf_pos = p->pos;
-
     int copy_size = count;
-    if (count > *buf_pos) copy_size = *buf_pos;
+    if (count > p->pos) copy_size = p->pos;
     if (copy_to_user(_buf, p->buffer, copy_size) != 0) {
         return -EFAULT;
     }
 
     *f_pos += copy_size;
     int i;
-    for (i = copy_size; i < *buf_pos; i++) {
+    for (i = copy_size; i < p->pos; i++) {
         p->buffer[i - copy_size] = p->buffer[i];
     }
-    *buf_pos -= copy_size;
+    p->pos -= copy_size;
 
     return count;
 }
@@ -104,13 +102,16 @@ static ssize_t mydevice_read(struct file *fp, char __user *_buf, size_t count, l
 static ssize_t mydevice_write(struct file *fp, const char __user *_buf, size_t count, loff_t *f_pos) {
     printk("write my device\n");
 
+    if (count > BUF_SIZE) {
+        return -EINVAL;
+    }
+
     struct _mydevice_file_data *p = fp->private_data;
     if (copy_from_user(p->buffer, _buf, count) != 0) {
         return -EFAULT;
     }
 
-    int *buf_pos = p->pos;
-    *buf_pos += count;
+    p->pos += count;
 
     return count;
 }
@@ -126,22 +127,18 @@ struct file_operations s_mydevice_fops = {
 static int mydevice_init(void) {
     printk("init my device\n");
 
-    dev_t dev;
     if (alloc_chrdev_region(&dev, MINOR_BASE, MINOR_NUM, DRIVER_NAME) != 0) {
         printk(KERN_ERR  "alloc_chrdev_region = %d\n", major_num);
         return -1;
     }
 
-    major_num = MAJOR(dev);
-    dev = MKDEV(major_num, MINOR_BASE);
-
-    cdev_init(&my_cdev,&s_mydevice_fops);
+    cdev_init(&my_cdev, &s_mydevice_fops);
     my_cdev.owner = THIS_MODULE;
     int cdev_err = cdev_add(&my_cdev, dev, MINOR_NUM);
     if (cdev_err != 0) {
         printk(KERN_ERR  "cdev_add = %d\n", cdev_err);
         unregister_chrdev_region(dev, MINOR_NUM);
-        return -1;
+        return -cdev_err;
     }
     return 0;
 }
@@ -149,7 +146,6 @@ static int mydevice_init(void) {
 static void mydevice_exit(void) {
     printk("exit my device\n");
 
-    dev_t dev = MKDEV(major_num, MINOR_BASE);
     cdev_del(&my_cdev);
     unregister_chrdev_region(dev, MINOR_NUM);
 }
